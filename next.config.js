3180a797b41052dfc80f351e6c2c14deaf96cf00
kafkapple <biasdrive@gmail.com>
2026-04-26 11:17:42 +0900

    Add Next.js configuration file next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */                           
+  const nextConfig = {                                   
+    output: 'export',                                              
+    basePath: '/nextjs-blog',
+    images: { unoptimized: true },                                   
+    trailingSlash: true,
+  };                                                                 
+                                                         
+  module.exports = nextConfig;  
